f85b8a96c5e4912ca31f429f968a3e936e6c00ea
hoopmad <hoopmad22@gmail.com>
2023-04-03 00:22:12 +0900

    created apps

--- ExcelCalc/ExcelCalc/settings.py
+++ ExcelCalc/ExcelCalc/settings.py
@@ -11,16 +11,28 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p9(57w1_a5kr(q&y0vp0g28@0gbsvo%yh&lo!yc!57vd5kj5@q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
